9ff598d084ab888383a6c424acb09954ccd37f16
Stan Bratuszewski <145658170+sbratuszewski@users.noreply.github.com>
2024-02-08 21:17:31 +0000

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$conn = mysqli_connect('db5012732574.hosting-data.io', 'dbu893484', 'RTuPgALTD9#', 'dbs10695921', '3306');
+
+if (mysqli_connect_errno()) {
+    echo 'Connection to mysqli failed' . mysqli_connect_error();
+    exit();
+}
+?>
